6f340a948dbec8b505f182780a00dd452fb51ade
unknown <2576591129@qq.com>
2023-03-26 12:08:43 +0800

    2023-3-26

---
+++ NLP实体识别系列课程/Bert_NER_LSTM.py
@@ -0,0 +1,172 @@
+import os
+from torch.utils.data import Dataset,DataLoader
+from transformers import BertTokenizer
+from transformers import BertModel
+from transformers import AdamW
+import torch
+import torch.nn as nn
+from seqeval.metrics import accuracy_score as seq_accuracy_score
+from seqeval.metrics import f1_score as seq_f1_score
+from seqeval.metrics import precision_score as seq_precision_score
+from seqeval.metrics import recall_score as seq_recall_score
+from torchcrf import CRF
+
+# from sklearn.metrics import accuracy_score as sklearn_accuracy_score
+# from sklearn.metrics import f1_score as sklearn_f1_score
+# from sklearn.metrics import precision_score as sklearn_precision_score
+# from sklearn.metrics import recall_score as sklearn_recall_score
+
+def read_data(file):
+    with open(file,"r",encoding="utf-8") as f:
+        all_data = f.read().split("\n")
+
+    all_text = []
+    all_label = []
+
+    text = []
+    label = []
+    for data in all_data:
+        if data == "":
+            all_text.append(text)
+            all_label.append(label)
+            text = []
+            label = []
+        else:
+            t,l = data.split(" ")
+            text.append(t)
+            label.append(l)
+
+    return all_text,all_label
+
+def build_label(train_label):
+    label_2_index = {"PAD":0,"UNK":1}
+    for label in train_label:
+        for l in label:
+            if l not in label_2_index:
+                label_2_index[l] = len(label_2_index)
+    return label_2_index,list(label_2_index)
+
+class BertDataset(Dataset):
+    def __init__(self,all_text,all_label,label_2_index,max_len,tokenizer):
+        self.all_text = all_text
+        self.all_label = all_label
+        self.label_2_index = label_2_index
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+
+    def __getitem__(self,index):
+        text = self.all_text[index]
+        label = self.all_label[index][:self.max_len]
+
+        text_index = self.tokenizer.encode(text,add_special_tokens=True,max_length=self.max_len+2,padding="max_length",truncation=True,return_tensors="pt")
+        label_index = [0] +  [self.label_2_index.get(l,1) for l in label] + [0] + [0] * (self.max_len - len(text))
+
+        label_index = torch.tensor(label_index)
+        return  text_index.reshape(-1),label_index,len(label)
+
+    def __len__(self):
+        return self.all_text.__len__()
+
+
+class Bert_LSTM_NerModel(nn.Module):
+    def __init__(self,lstm_hidden,class_num):
+        super().__init__()
+
+        self.bert = BertModel.from_pretrained(os.path.join("..","bert_base_chinese"))
+        for name,param in self.bert.named_parameters():
+            param.requires_grad = False
+
+        self.lstm = nn.LSTM(768,lstm_hidden,batch_first=True,num_layers=1,bidirectional=False) # 768 * lstm_hidden
+        self.classifier = nn.Linear(lstm_hidden,class_num)
+
+        self.crf = CRF(class_num,batch_first=True)
+        # self.loss_fun = nn.CrossEntropyLoss()
+
+    def forward(self,batch_index,batch_label=None):
+        bert_out = self.bert(batch_index)
+        bert_out0,bert_out1 = bert_out[0],bert_out[1]# bert_out0:字符级别特征, bert_out1:篇章级别
+
+        lstm_out,_ = self.lstm(bert_out0)
+
+        pre = self.classifier(lstm_out)
+        if batch_label is not None:
+            # loss = self.loss_fun(pre.reshape(-1,pre.shape[-1]),batch_label.reshape(-1))
+            loss = -self.crf(pre,batch_label)
+            return loss
+        else:
+
+            pre = self.crf.decode(pre)
+            return pre
+
+
+
+if __name__ == "__main__":
+
+
+    train_text, train_label = read_data(os.path.join("data","train.txt"))
+    dev_text,dev_label = read_data(os.path.join("data","dev.txt"))
+    test_text,test_label = read_data(os.path.join("data","test.txt"))
+
+    label_2_index,index_2_label = build_label(train_label)
+
+    tokenizer = BertTokenizer.from_pretrained(os.path.join("..","bert_base_chinese"))
+
+    batch_size = 50
+    epoch = 100
+    max_len = 30
+    lr = 0.0005
+    lstm_hidden = 128
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    train_dataset = BertDataset(train_text,train_label,label_2_index,max_len,tokenizer)
+    train_dataloader = DataLoader(train_dataset,batch_size=batch_size,shuffle=False)
+
+    dev_dataset = BertDataset(dev_text, dev_label, label_2_index, max_len, tokenizer)
+    dev_dataloader = DataLoader(dev_dataset, batch_size=batch_size, shuffle=False)
+
+    model = Bert_LSTM_NerModel(lstm_hidden,len(label_2_index)).to(device)
+    opt = AdamW(model.parameters(),lr)
+
+    for e in range(epoch):
+        model.train()
+        for batch_text_index,batch_label_index,batch_len in train_dataloader:
+            batch_text_index = batch_text_index.to(device)
+            batch_label_index = batch_label_index.to(device)
+            loss = model.forward(batch_text_index,batch_label_index)
+            loss.backward()
+
+            opt.step()
+            opt.zero_grad()
+
+            print(f'loss:{loss:.2f}')
+
+        model.eval()
+
+        all_pre = []
+        all_tag = []
+        for batch_text_index,batch_label_index,batch_len in dev_dataloader:
+            batch_text_index = batch_text_index.to(device)
+            batch_label_index = batch_label_index.to(device)
+            pre = model.forward(batch_text_index)
+
+            # pre = pre.cpu().numpy().tolist()
+            tag = batch_label_index.cpu().numpy().tolist()
+
+            for p,t,l in zip(pre,tag,batch_len):
+                p = p[1:1+l]
+                t = t[1:1+l]
+
+                p = [index_2_label[i] for i in p]
+                t = [index_2_label[i] for i in t]
+
+                all_pre.append(p)
+                all_tag.append(t)
+
+        f1_score = seq_f1_score(all_tag,all_pre)
+        print(f"f1:{f1_score}")
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -3,78 +3,80 @@
 
 ## 最近我们出了[NLP全程班系列](#anchor1)哦，从基础到入门到企业级项目，长达4个月的课程，低至299！，感兴趣添加vx：shouxie_ai，获取课程列表和报名哈
 
-
 ## 免费系列课程
 
 #### NLP基础及深度学习基础，谁看谁明白：
+
 视频：https://www.bilibili.com/video/BV1j3411t7VM?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/A-series-of-NLP/tree/main/NLP_%E6%B7%B1%E5%BA%A6%E5%AD%A6%E4%B9%A0_%E5%9F%BA%E7%A1%80%E8%AF%BE%E7%A8%8B
 
 #### NLP文本分类系列课程：
+
 视频：https://www.bilibili.com/video/BV1844y1V7sM?p=1&vd_source=3dbbba63d1589615f19db07d062f03b0
 
 代码：https://github.com/shouxieai/A-series-of-NLP/tree/main/%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB
 
-
 #### NLP实体识别系列课程，更新至 Bert + LSTM + CRF：
+
 视频：https://www.bilibili.com/video/BV1X34y1V7VX?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/A-series-of-NLP/tree/main/%E5%AE%9E%E4%BD%93%E8%AF%86%E5%88%AB
 
-
 #### pytorch_BiLSTM 命名实体识别 ：
+
 视频：https://www.bilibili.com/video/BV1h341187RA?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/nlp-bilstm_crf-ner
 
+#### NLP模型复现之TextCNN：手写AI出品:
 
-#### NLP模型复现之TextCNN：手写AI出品: 
 视频：https://www.bilibili.com/video/BV163411w7qg?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/A-series-of-NLP/tree/main/%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB/TextCNN_%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB
 
-
 #### NLP模型复现之word2vec：
+
 视频：https://www.bilibili.com/video/BV1VP4y1s7eS?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/word_2_vec
 
-
 #### NLP模型复现之HMM：
+
 视频：https://www.bilibili.com/video/BV1aP4y147gA?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/nlp-hmm-word-cut
 
-
 #### Seq2Seq 机器翻译：
+
 视频：https://www.bilibili.com/video/BV1hf4y1u7ez?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/seq2seq_translation
 
-
 #### NLP LSTM模型古诗生成：
+
 视频：https://www.bilibili.com/video/BV1sM4y1N7ov?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/LSTM_poetry_generate
 
-
 #### 百度AI NLP功能快速上手：
+
 视频：https://www.bilibili.com/video/BV15o4y1Q7wP?spm_id_from=333.999.0.0
 
 代码：https://github.com/shouxieai/BaiduAI_SR_sent
 
-
 #### NLP中正则表达式应用：2022年8月27日免费直播，
+
 录播地址：https://www.bilibili.com/video/BV13T411F7bM?spm_id_from=333.999.0.0&vd_source=3dbbba63d1589615f19db07d062f03b0
 
 代码：https://github.com/shouxieai/A-series-of-NLP/tree/main/2022_8_27_%E6%AD%A3%E5%88%99%E7%9B%B4%E6%92%AD%E4%BB%A3%E7%A0%81_%E8%B5%84%E6%96%99
 
-
-
 ------------------------------------
+
 ## 付费直播（录播）短期系列课程
+
 一、基于Bert文本分类 & Bert Embedding嫁接！
+
 1. NLP发展现状，入门NP算法工程师需要的技能
 2. Bert模型各文件解析，Bert文本分类流程讲解
 3. 基于Hugging-face Bert文本分类代码逐行手写
@@ -82,20 +84,20 @@
 
 价格29.9  私聊微信：shouxie_ai，获取课程链接及代码，答疑群永久维护
 
- 
- 
 二、 基于RNN+Bert的实体识别及文本分类联合任务
+
 1. 简易爬虫：爬取中文人名、武侠小说
 2. 数据准备：数据标注策略、训练策略及数据增强手段
-2. 模型复现：使用pytorch逐行复现RNN循环神经网络
-3. 模型拼接：多任务与多模型拆解，逐行手写代码，手把手完成任务
+3. 模型复现：使用pytorch逐行复现RNN循环神经网络
+4. 模型拼接：多任务与多模型拆解，逐行手写代码，手把手完成任务
 
 价格29.9  私聊微信：shouxie_ai，私聊获取课程链接及代码，答疑群永久维护
 
  ------------------------------------
- 
+
  <a name="anchor1"></a>
- ## NLP全程班系列：
+
+## NLP全程班系列：
 
 课堂代码逐行手写！微信群全天候答疑！前11节课、第48节课transformer逐行复现 免费试听~
 
@@ -106,9 +108,6 @@ https://www.bilibili.com/video/BV1Gt4y1w7SV/?spm_id_from=333.788
 https://www.bilibili.com/video/BV1BP4y1Q7Lg/ <br>
 代码：https://github.com/shouxieai/A-series-of-NLP/tree/main/%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB/Transformer%E6%96%87%E6%9C%AC%E5%88%86%E7%B1%BB
 
-
 截至2022年12月底，全程班课程内容如下：
 ![image](https://user-images.githubusercontent.com/87959426/209346323-a0cd2cf0-ef82-421b-b3f5-d70fe472fb85.png)
 微信：shouxie_ai，私聊获取全部课程大纲，学生价低至299！超长课时，细到极致，不怕你听不懂！
-
-
